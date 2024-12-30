5f971a38ab77b3fe119b4559ecea429511df4fff
Crowdbotics <team@crowdbotics.com>
2024-12-30 12:05:16 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "falling-union-50529",
+  url: "https://falling-union-50529.botics.co",
+  api: "https://falling-union-50529.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
